82da7ba9fe2041e45c7f0fd8a5e39284a5a82aed
ichristwin <ichristwin@gmail.com>
2023-11-25 01:58:06 +0100

    interface fixed

--- contracts/interfaces/IProtocol.sol
+++ contracts/interfaces/IProtocol.sol
@@ -2,9 +2,13 @@
 pragma solidity ^0.8.19;
 
 interface Strategy {
-  function claim(uint256 revenueAmount, address receiver, uint256 outputAmount, uint256 deadline) external;
+    function claim(
+        uint256 revenueAmount,
+        address receiver,
+        uint256 outputAmount,
+        uint256 deadline
+    ) external;
 }
-
 
 interface IProtocol {
     error BadStrategy();
@@ -50,7 +54,12 @@ interface IProtocol {
 
     function pay(uint256 tokenId, uint256 amount) external;
 
-    function claim(address libAddress, address receiver, uint256 outputAmount, uint256 deadline) external;
+    function claim(
+        address libAddress,
+        address receiver,
+        uint256 outputAmount,
+        uint256 deadline
+    ) external;
 
     function stateOf(uint256 tokenId) external view returns (bool);
 
